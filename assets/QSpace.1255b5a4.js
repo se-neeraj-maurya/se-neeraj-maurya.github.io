be15ee7b169eae719ce91c0e612dcd800b652985
Neeraj Maury <67846720+neerajmauryagpj@users.noreply.github.com>
2024-09-30 20:18:54 +0530

    new build

--- assets/QSpace.1255b5a4.js
+++
@@ -1,1 +0,0 @@
-import{h as e,y as a}from"./index.ca18b857.js";const p=e("div",{class:"q-space"});var c=a({name:"QSpace",setup(){return()=>p}});export{c as Q};
---
+++ assets/use-dark.8c6937cb.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index.bbb8bb2c.js";const u={dark:{type:Boolean,default:null}};function o(a,r){return e(()=>a.dark===null?r.dark.isActive:a.dark)}export{o as a,u};
